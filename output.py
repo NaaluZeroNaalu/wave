1b3e18b28e57e52a3fed9907aef3eb22b1d8f2b1
NaaluZeroNaalu <126253328+NaaluZeroNaalu@users.noreply.github.com>
2025-04-16 10:45:46 +0530

    Update output.py

--- output.py
+++ output.py
@@ -7,7 +7,7 @@ pages = {
         st.Page("ncr.py",title="NCR Report",icon=":material/dataset:"),
         st.Page("structure_and_finishing_main.py",title="Structure and Finishing",icon=":material/view_timeline:"),
         st.Page("shedule_report.py",title="Flatwise shedule report",icon=":material/apartment:"),
-        st.Page("safety.py",title="Safety report",icon=":material/engineering:"),
+        st.Page("Safety.py",title="Safety report",icon=":material/engineering:"),
         st.Page("house.py",title="House report",icon=":material/home:"),
     ]
 }
