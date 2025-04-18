cc54a669e7be2518817ea56d4d5c502e98416355
NaaluZeroNaalu <ajithruflex1210@gmail.com>
2025-04-18 14:19:38 +0530

    removed test

--- output.py
+++ output.py
@@ -5,11 +5,10 @@ pages = {
     "REPORTS":[
         st.Page("dashboard.py",title="Dashboard",icon=":material/dashboard:"),
         st.Page("ncr.py",title="NCR Report",icon=":material/dataset:"),
-        st.Page("structure_and_finishing_main.py",title="Structure and Finishing",icon=":material/view_timeline:"),
-        st.Page("shedule_report.py",title="Flatwise shedule report",icon=":material/apartment:"),
+        st.Page("structure_and_finishing_main.py",title="Overall Project Completion - Structure and Finishing",icon=":material/view_timeline:"),
+        st.Page("shedule_report.py",title="Flatwise report",icon=":material/apartment:"),
         st.Page("safety.py",title="Safety report",icon=":material/engineering:"),
         st.Page("house.py",title="House report",icon=":material/home:"),
-        st.Page("testcos.py",title="Test",icon=":material/home:")
     ]
 }
 
